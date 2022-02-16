259cf2c4a4cfa287b9e8904b40b7c722175f0f33
Vlademir Lemos <vlademir.consultex@gmail.com>
2022-02-16 20:23:53 -0300

    adicionando projeto inicial

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationVlademirApplication.java
@@ -0,0 +1,15 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationVlademirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationVlademirApplication.class, args);
+	}
+
+}
+
+
