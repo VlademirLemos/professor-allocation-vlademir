f93e7f25ff646e12a371d663cc4c79d5d4da22bc
Vlademir Lemos <vlademir.consultex@gmail.com>
2022-02-21 13:48:26 -0300

    Geração Automática de Identificadores

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,61 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+	@Column(length = 14, unique = true, nullable = false)
+	private String cpf;
+
+	@Column(name = "department_id", nullable = false)
+	private Long departmentId;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "department_id", updatable = false, insertable = false, nullable = false)
+	private Department depart;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+}
